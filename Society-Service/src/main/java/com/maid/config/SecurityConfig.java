8a82cb4081cf8745c90a43b364d8e5cfb238986f
Lokesh Rachha <Lokesh.Rachha@cognizant.com>
2019-10-23 22:27:53 +0530

    basic auth

---
+++ Society-Service/src/main/java/com/maid/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.maid.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+		auth.inMemoryAuthentication().withUser("loki").password(encoder.encode("loki")).roles("USER", "ADMIN").and()
+				.withUser("test").password(encoder.encode("test")).roles("USER");
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/").permitAll().antMatchers("/society").hasAnyRole("USER", "ADMIN")
+				.antMatchers("/societies").hasAnyRole("ADMIN").anyRequest().authenticated().and().formLogin()
+				.permitAll().and().logout().permitAll();
+
+		http.csrf().disable();
+	}
+
+}
--- Society-Service/src/main/java/com/maid/controller/SocietyController.java
+++ Society-Service/src/main/java/com/maid/controller/SocietyController.java
@@ -1,6 +1,5 @@
 package com.maid.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,12 +8,15 @@ import com.maid.properties.SocietyProperties;
 @RestController
 public class SocietyController {
 
-	@Autowired
-	private SocietyProperties societyProperties;
+	private final SocietyProperties societyProperties;
+
+	public SocietyController(SocietyProperties societyProperties) {
+		this.societyProperties = societyProperties;
+	}
 
 	@GetMapping("/societies")
 	public String getSocieties() {
 		return societyProperties.getSocieties();
 	}
-	
+
 }
